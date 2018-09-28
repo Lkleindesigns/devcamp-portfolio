f4e22922975e8e66cd081879cfb23e3e8938b81c
Logan Klein <Lkleindesigns@gmail.com>
2018-09-28 09:06:21 -0500

    Update custom scopes for data feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
     Blog.create!(
         title: "My blog post #{blog}",
@@ -6,7 +14,8 @@
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
         ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
         in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        topic_id: Topic.last.id
     )
 end
 
@@ -21,10 +30,10 @@ end
 
 puts "5 skill created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
-        title: "Portfolio title #{portfolio_item}",
-        subtitle: "my great service",
+        title: "Portfolio Item #{portfolio_item}",
+        subtitle: "Ruby on Rails",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
@@ -34,4 +43,20 @@ puts "5 skill created"
     )
 end
 
-puts "9 portfolio items created"
+puts "8 portfolio items created"
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio Item #{portfolio_item}",
+        subtitle: "React",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+        ut aliquip ex ea commodo consequat.",
+        main_image: "https://placehold.it/600x400",
+        thumb_image: "https://placehold.it/350x200"   
+    )
+end
+
+
+puts "1 portfolio item created"
