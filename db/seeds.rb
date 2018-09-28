30d1d90faf02148eea7e3e38b2b1f50fd621aee3
Logan Klein <Lkleindesigns@gmail.com>
2018-09-28 09:41:59 -0500

    Integrate technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -60,3 +60,11 @@ end
 
 
 puts "1 portfolio item created"
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}"
+    )
+end
+
+puts "3 technologies created"
