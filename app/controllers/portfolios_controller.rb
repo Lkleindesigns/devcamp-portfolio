d993f7f9515375d97159ec49104873aa5350e5fb
Logan Klein <Lkleindesigns@gmail.com>
2018-10-06 17:18:59 -0500

    Refactor strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,64 +1,72 @@
 class PortfoliosController < ApplicationController
-    def index
-        @portfolio_items = Portfolio.all
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def react
+    @react_portfolio_items = Portfolio.react
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def react
-        @react_portfolio_items = Portfolio.react
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
     end
+  end
 
-    def new
-        @portfolio_item = Portfolio.new
-        3.times { @portfolio_item.technologies.build }
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy the record
+    @portfolio_item.destroy
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
     end
+  end
 
-    def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-        technologies_attributes: [:name]))
+  private
 
-        respond_to do |format|
-            if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-            format.json { render :show, status: :created, location: @portfolio_item }
-            else
-            format.html { render :new }
-            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-            end
-        end
-    end
-
-    def edit
-        @portfolio_item = Portfolio.find(params[:id])
-    end
-
-    def update
-        @portfolio_item = Portfolio.find(params[:id])
-
-        respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-            format.json { render :show, status: :ok, location: @portfolio_item }
-          else
-            format.html { render :edit }
-            format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-          end
-        end
-    end
-
-    def show
-        @portfolio_item = Portfolio.find(params[:id])
-    end
-
-    def destroy
-        # Perform the lookup
-        @portfolio_item = Portfolio.find(params[:id])
-
-        # Destroy the record
-        @portfolio_item.destroy
-
-        # Redirect
-        respond_to do |format|
-          format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully destroyed.' }
-        end
-    end
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name])
+  end
 end
