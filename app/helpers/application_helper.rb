a1151e7222a4fa205a56b57536db027105ae6d44
Logan Klein <Lkleindesigns@gmail.com>
2018-10-29 21:37:07 -0500

    Update call to rubygem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    KleinViewTool::Renderer.copyright 'Logan Klein', 'All rights reserved'
+  end
 end
